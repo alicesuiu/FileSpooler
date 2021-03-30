f87a4164f7680c9631a652d4ddc2dbf9e7b7ef17
Alice Suiu <alicesuiu17@gmail.com>
2021-03-30 13:28:38 +0300

    Add script that generates files with random data

---
+++ generate_files.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check number of parameters
+if [[ "$#" -lt 3 ]]; then
+	echo "[Usage]: ./generate_files.sh <number_of_files> <file_size> <path_to_files>"
+	exit 1
+fi
+
+# Check first parameters
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+	echo "First parameter is not a valid number!"
+	echo "[Usage]: ./generate_files.sh <val> p2 p3, val is integer, gt 0"
+	exit 1
+fi
+
+# Check second parameter
+if ! [[ $2 =~ ^[0-9]+(.[0-9]+)?$ ]]; then
+	echo "Second parameter is not a valid number!"
+	echo "[Usage]: ./generate_files.sh p1 <val> p3, val is numerical, gt 0"
+	exit 1
+fi
+
+# Check third parameter
+if ! [[ -d $3 ]]; then
+	echo "Third parameter is not a valid directory!"
+	exit 1
+fi
+
+# Generating files
+for ((i = 0; i < $1; i++)); do
+	filename=file_$i
+	echo $i $filename
+	dd if=/dev/urandom of=$3/$filename bs=$2M count=1
+done
