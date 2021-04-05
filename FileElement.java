e5d50d388d31533d1dcd2b02af369ca1e4320eaf
Alice Suiu <alicesuiu17@gmail.com>
2021-04-05 22:56:09 +0300

    Update Spooler class
    Add computation of xxHash64 checksum for validation of transmitted data

--- FileElement.java
+++ FileElement.java
@@ -1,36 +1,26 @@
+import java.io.File;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
 public class FileElement implements Delayed {
-    private final String fileName;
-    private final long fileSize;
-    private final String absolutePath;
+    private final File file;
     private int nrTries;
     private long time;
     private String md5;
+    private String xxhash;
 
-    public FileElement(String fileName, long fileSize, String absolutePath) {
-        this.fileName = fileName;
-        this.fileSize = fileSize;
-        this.absolutePath = absolutePath;
+    public FileElement(File file) {
+        this.file = file;
         nrTries = 0;
         time = System.currentTimeMillis();
     }
 
-    public String getFileName() {
-        return fileName;
-    }
-
-    public long getFileSize() {
-        return fileSize;
+    public File getFile() {
+        return file;
     }
 
     public int getNrTries() {
         return nrTries;
-    }
-
-    public String getAbsolutePath() {
-        return absolutePath;
     }
 
     public long getTime() {
@@ -39,6 +29,14 @@ public class FileElement implements Delayed {
 
     public String getMd5() {
         return md5;
+    }
+
+    public String getXxhash() {
+        return xxhash;
+    }
+
+    public void setXxhash(String xxhash) {
+        this.xxhash = xxhash;
     }
 
     public void setMd5(String md5) {
@@ -56,9 +54,9 @@ public class FileElement implements Delayed {
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("FileElement{");
-        sb.append("fileName='").append(fileName).append('\'');
-        sb.append(", fileSize=").append(fileSize);
-        sb.append(", absolutePath='").append(absolutePath).append('\'');
+        sb.append("fileName='").append(file.getName()).append('\'');
+        sb.append(", fileSize=").append(file.length());
+        sb.append(", absolutePath='").append(file.getAbsolutePath()).append('\'');
         sb.append(", nrTries=").append(nrTries);
         sb.append(", time=").append(time);
         sb.append('}');
--- Spooler.java
+++ Spooler.java
@@ -1,23 +1,19 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import alien.config.ConfigUtils;
+import alien.io.IOUtils;
+import net.jpountz.xxhash.StreamingXXHash64;
+import net.jpountz.xxhash.XXHashFactory;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.DelayQueue;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Spooler {
     private BlockingQueue<FileElement> filesToSend;
-    private List<FileElement> md5ToRegister;
-    private final String eosServerPath;
-    private final String eosDirPath;
+    private List<FileElement> md5Checksums;
+    private List<FileElement> xxHashChecksums;
     private final String sourceDirPath;
     private final Boolean md5Option;
     private final int maxBackoff;
@@ -27,26 +23,24 @@ public class Spooler {
     private final int badTransfer = 1;
     private final int successfulTransfer = 0;
 
-    public Spooler(String eosServerPath, String eosDirPath, String sourceDirPath, int maxBackoff, Boolean md5Option) {
-        this.eosServerPath = eosServerPath;
-        this.eosDirPath = eosDirPath;
+    public Spooler(String sourceDirPath, int maxBackoff, Boolean md5Option) {
         this.sourceDirPath = sourceDirPath;
         this.maxBackoff = maxBackoff;
         this.md5Option = md5Option;
+        md5Checksums = new ArrayList<>();
+        xxHashChecksums = new ArrayList<>();
+        logger = ConfigUtils.getLogger(Spooler.class.getCanonicalName());
         filesToSend = addFilesToSend();
         FileWatcher watcher = new FileWatcher((new File(sourceDirPath))).addListener(new FileAdapter() {
             @Override
             public void onCreated(FileEvent event) {
-                filesToSend.add(new FileElement(event.getFile().getName(),
-                        event.getFile().length(),
-                        event.getFile().getAbsolutePath()));
-                System.out.println(filesToSend);
+                FileElement file = new FileElement(event.getFile());
+                filesToSend.add(file);
+                xxHashChecksum(file);
+                //System.out.println(filesToSend);
             }
         });
         watcher.watch();
-        System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tF %1$tT] [%4$-2s] %5$s %n");
-        logger = Logger.getLogger(Spooler.class.getName());
-        md5ToRegister = new ArrayList<>();
     }
 
     private BlockingQueue<FileElement> addFilesToSend() {
@@ -58,10 +52,11 @@ public class Spooler {
             filesToSend = new DelayQueue<>();
 
             for (i = 0; i < files.length; i++) {
-                filesToSend.add(new FileElement(files[i].getName(),
-                        files[i].length(),
-                        files[i].getAbsolutePath()));
+                filesToSend.add(new FileElement(files[i]));
             }
+
+            for (FileElement file : filesToSend)
+                xxHashChecksum(file);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
@@ -73,71 +68,35 @@ public class Spooler {
         return filesToSend;
     }
 
-    private void send(FileElement file) throws IOException, InterruptedException {
-        boolean transfer;
-        long timeWaitSend;
-        ProcessBuilder shellProcess;
-        Process process;
-        ProcessWithTimeout ptimeout;
-        List<String> cmd = new ArrayList<>();
-
-        cmd.add("eos");
-    	cmd.add(eosServerPath);
-    	cmd.add("cp");
-        cmd.add("file:" + file.getAbsolutePath());
-        cmd.add(eosDirPath + "/" + file.getFileName());
-
-        shellProcess = new ProcessBuilder();
-        shellProcess.command(cmd);
-        process = shellProcess.start();
-
-        ptimeout = new ProcessWithTimeout(process, shellProcess);
-        timeWaitSend = getWaitTimeSend(file);
-        transfer = ptimeout.waitFor(timeWaitSend, TimeUnit.SECONDS);
-
-        if (!transfer)
-            checkTransferStatus(badTransfer, file);
-        else
-            checkTransferStatus(successfulTransfer, file);
-    }
-
-    private long getWaitTimeSend(FileElement file) {
-        long bandwith;
-
-        bandwith = (100000 << 3) / Main.nrFilesOnSend.get() / (file.getNrTries() + 1);
-        return file.getFileSize() / bandwith;
-    }
-
-    private void checkTransferStatus(int exitCode, FileElement file) {
+    private void checkTransferStatus(int exitCode, FileElement element, String xxhash) {
         long delayTime;
 	int badTransferDelayTime = 10;
 
-        if (exitCode == successfulTransfer) {
+        if (exitCode == successfulTransfer && element.getXxhash().equals(xxhash)) {
             Main.nrFilesSent.getAndIncrement();
-            logger.log(Level.INFO, "The " + file.getFileName() + " file is successfully sent!");
+            logger.log(Level.INFO, "The " + element.getFile().getName() + " file is successfully sent!");
             logger.log(Level.INFO, "Total number of files successfully transferred: " + Main.nrFilesSent.get());
             if (md5Option)
-                checksum(file);
-            deleteFile(file);
+                md5Checksum(element);
+            Eos.delete(element);
         } else {
             Main.nrFilesFailed.getAndIncrement();
-            logger.log(Level.WARNING, "Transmission of the " + file.getFileName() + " file failed!");
+            logger.log(Level.WARNING, "Transmission of the " + element.getFile().getName() + " file failed!");
             logger.log(Level.INFO, "Total number of files whose transmission failed: " + Main.nrFilesFailed.get());
-            file.setNrTries(file.getNrTries() + 1);
-            delayTime = Math.min(Math.max((1 << file.getNrTries()), badTransferDelayTime), maxBackoff);
+            element.setNrTries(element.getNrTries() + 1);
+            delayTime = Math.min(Math.max((1 << element.getNrTries()), badTransferDelayTime), maxBackoff);
             logger.log(Level.INFO, "The delay time of the file is: " + delayTime);
-            file.setTime(System.currentTimeMillis() + delayTime * 1000);
-            logger.log(Level.INFO, "The transmission time of the file is: " + file.getTime());
-            filesToSend.add(file);
+            element.setTime(System.currentTimeMillis() + delayTime * 1000);
+            logger.log(Level.INFO, "The transmission time of the file is: " + element.getTime());
+            filesToSend.add(element);
         }
-    }
-
-    private void deleteFile(FileElement file) {
-        new File(file.getAbsolutePath()).delete();
     }
 
     public void run() {
         FileElement file;
+        EosCommand command;
+        String xxhash;
+
         try {
             while (true) {
                 logger.log(Level.INFO, "Total number of files to be transferred: " + filesToSend.size());
@@ -147,7 +106,15 @@ public class Spooler {
                 logger.log(Level.INFO, "Total number of files transmitted in parallel: "
                         + Main.nrFilesOnSend.getAndIncrement());
 
-                send(file);
+                command = Eos.transfer(file);
+                xxhash = command.getOutput().toString().split(" ")[3].replace("\n", "");
+                logger.log(Level.INFO, "Received xxhash checksum: " + xxhash + " for "
+                        + file.getFile().getName());
+
+                if (!command.isStatus())
+                    checkTransferStatus(badTransfer, file, xxhash);
+                else
+                    checkTransferStatus(successfulTransfer, file, xxhash);
 
                 Main.nrFilesOnSend.getAndDecrement();
             }
@@ -156,27 +123,46 @@ public class Spooler {
         }
     }
 
-    private void checksum(FileElement file) {
+    private void md5Checksum(FileElement element) {
         String checksum;
-        int md5BufferSize = 8192;
 
         try {
-            FileInputStream fis = new FileInputStream(file.getAbsolutePath());
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            checksum = IOUtils.getMD5(element.getFile());
+            element.setMd5(checksum);
+            md5Checksums.add(element);
+            logger.log(Level.INFO, "MD5 checksum for the file " + element.getFile().getName()
+                    + " is " + element.getMd5());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-            byte[] buffer = new byte[md5BufferSize];
-            int numOfBytesRead;
-            while ((numOfBytesRead = fis.read(buffer)) > 0) {
-                md5.update(buffer, 0, numOfBytesRead);
+    private void xxHashChecksum(FileElement element) {
+        long checksum;
+        XXHashFactory factory;
+        File file = element.getFile();
+        BufferedInputStream buffStream;
+        InputStream input;
+
+        try {
+            input = new FileInputStream(file);
+            buffStream = new BufferedInputStream(input);
+            factory = XXHashFactory.fastestInstance();
+            StreamingXXHash64 hash64 = factory.newStreamingHash64(0);
+            byte[] buffer = new byte[8192];
+            for (;;) {
+                int read = buffStream.read(buffer);
+                if (read == -1) {
+                    break;
+                }
+                hash64.update(buffer, 0, read);
             }
-            byte[] hash = md5.digest();
-            checksum = new BigInteger(1, hash).toString(16);
-
-            file.setMd5(checksum);
-            md5ToRegister.add(file);
-            logger.log(Level.INFO, "MD5 checksum for the file " + file.getFileName() + " is " + file.getMd5());
-
-        } catch (NoSuchAlgorithmException | IOException e) {
+            checksum = hash64.getValue();
+            element.setXxhash(String.format("%016x", checksum));
+            xxHashChecksums.add(element);
+            logger.log(Level.INFO, "xxHash64 checksum for the file " + element.getFile().getName()
+                    + " is " + element.getXxhash());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
