31a313eb9d2a3171577de8c4c980d2b74d518068
Alice Suiu <alicesuiu17@gmail.com>
2021-10-15 00:50:41 +0300

    Add XRD_WRITERECOVERY to the epn2eos_start script
    Signed-off-by: Alice Suiu <alicesuiu17@gmail.com>

--- scripts/epn2eos_start.sh
+++ scripts/epn2eos_start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-/etc/alternatives/jre_16/bin/java -Xms1G -Xmx1G -cp /home/jalien/alien-cs.jar:/home/jalien/spooler.jar -DAliEnConfig=/home/jalien/.alien/config spooler.Main &> /home/jalien/epn2eos_logs/$(hostname)/service.log
+if [ ! -d "/home/jalien/epn2eos_logs/$(hostname)" ]
+then
+  mkdir /home/jalien/epn2eos_logs/$(hostname)
+fi
 
+XRD_WRITERECOVERY=false /etc/alternatives/jre_16/bin/java -Xms1G -Xmx1G -cp /home/jalien/alien-cs.jar:/home/jalien/spooler_v.1.5.jar -DAliEnConfig=/home/jalien/.alien/config spooler.Main &> /home/jalien/epn2eos_logs/$(hostname)/service.log
