91975c4860abdb2a1f96ee45d1a706b85a453f3e
Alice Suiu <alicesuiu17@gmail.com>
2021-10-15 00:51:29 +0300

    Update simulate script
    Signed-off-by: Alice Suiu <alicesuiu17@gmail.com>

--- scripts/simulate_epn2eos.py
+++ scripts/simulate_epn2eos.py
@@ -21,27 +21,20 @@ def main():
     ]
     sizes = [1048576, 2097152, 3145728] #, 4194304, 5242880]
 
-    totalFiles = 0
-    totalBytes = 0
+    # ./spooler_scripts/generate_files.sh 1 1048576 /data/epn2eos_tool/epn2eos/
+    nrFiles = 1
+    dataDirIndex = random.randint(0, 4)
+    sizeIndex = 0
 
-    while True:
-        # ./spooler_scripts/generate_files.sh 1 1048576 /data/epn2eos_tool/epn2eos/
-        nrFiles = random.randint(1, 5)
-        totalFiles += nrFiles
-        dataDirIndex = random.randint(0, 4)
-        sizeIndex = random.randint(0, 2)
-        totalBytes += sizes[sizeIndex] * nrFiles
+    print('nrFiles: {}, dataDir: {}, size: {}'.format(str(nrFiles), dataDirs[dataDirIndex], str(sizes[sizeIndex])))
 
-        print('nrFiles: {}, dataDir: {}, size: {}'.format(str(nrFiles), dataDirs[dataDirIndex], str(sizes[sizeIndex])))
-        print('totalFile: {}'.format(str(totalFiles)))
-        print('totalBytes: {}'.format(str(totalBytes)))
+    generateFiles = "/home/jalien/spooler_scripts/generate_files.sh " + str(nrFiles) + " " + str(sizes[sizeIndex]) + " " + dataDirs[dataDirIndex]
+    os.system(generateFiles)
 
-        generateFiles = "/home/jalien/spooler_scripts/generate_files.sh " + str(nrFiles) + " " + str(sizes[sizeIndex]) + " " + dataDirs[dataDirIndex]
-        os.system(generateFiles)
+    # python3 ./spooler_scripts/generate_metafiles.py /data/epn2eos_tool/dataDir/1 /data/epn2eos_tool/epn2eos/
+    generateMeta = "python3 /home/jalien/spooler_scripts/generate_metafiles.py " + dataDirs[dataDirIndex] + " " + metadataDir
+    os.system(generateMeta)
 
-        # python ./spooler_scripts/generate_metafiles.py /data/epn2eos_tool/dataDir/1 /data/epn2eos_tool/epn2eos/
-        generateMeta = "python /home/jalien/spooler_scripts/generate_metafiles.py " + dataDirs[dataDirIndex] + " " + metadataDir
-        os.system(generateMeta)
 
 if __name__ == "__main__":
     main()
